9f3189fb17bf4f4d71da8c25425edcd49cdbfc12
Jose <acevedoflores.jose@gmail.com>
2012-12-11 19:03:22 -0400

    a

---
+++ ItunesStreamServer/src/acv2server/apps/IS/main/LibraryObject.java
@@ -0,0 +1,12 @@
+package acv2server.apps.IS.main;
+
+/**
+ * This class provides methods for accessing the Itunes Library file (C:\localhost\Music\Itunes\"Itunes Music Library.xml") 
+ * @author Jose
+ *
+ */
+public class LibraryObject {
+
+	
+	
+}
